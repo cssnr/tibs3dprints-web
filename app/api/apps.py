a7b41df6602c7b716adc614f27d284c6a2eec813
Shane <6071159+smashedr@users.noreply.github.com>
2025-06-12 21:59:46 -0700

    Update Names

--- app/api/apps.py
+++ app/api/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     name = "api"
-    verbose_name = "api"
+    verbose_name = "API"
 
     # def ready(self):
     #     import api.signals  # noqa: F401
