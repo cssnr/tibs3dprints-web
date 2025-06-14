9c1e34b0331afc2d7042f2933a954ff33e30bc72
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-06-13 17:47:23 -0700

    Update Models (#3)
    * Update Models
    
    * Update Home

--- app/home/admin.py
+++ app/home/admin.py
@@ -1,11 +1,66 @@
 from django.contrib import admin
 
-from .models import Contact, Message, MyNews
+from .models import BetaUser, Contact, Message, MyNews, TikTokUser
+
+
+admin.site.site_header = "Tibs3DPrints Administration"
 
 
 admin.site.register(Message)
 
-admin.site.site_header = "Tibs3DPrints Administration"
+
+@admin.action(description="Add selected to Beta Test")
+def mark_as_added(modeladmin, request, queryset):
+    queryset.update(added=True)
+
+
+@admin.register(TikTokUser)
+class TikTokUserAdmin(admin.ModelAdmin):
+    list_display = ("display_name", "updated_at", "created_at")
+    list_filter = ("display_name",)
+    search_fields = ("display_name",)
+    ordering = ("display_name",)
+    readonly_fields = (
+        "display_name",
+        "open_id",
+        "avatar_url",
+        "updated_at",
+        "created_at",
+        "access_token",
+        "refresh_token",
+        "expires_in",
+    )
+
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(BetaUser)
+class BetaUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "added",
+        "email",
+        "name",
+        "created_at",
+    )
+    list_filter = (
+        "added",
+        "email",
+        "name",
+    )
+    search_fields = (
+        "email",
+        "name",
+    )
+    list_display_links = ("email",)
+    ordering = ("-created_at",)
+    actions = [mark_as_added]
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Contact)
@@ -33,7 +88,12 @@ class ContactAdmin(admin.ModelAdmin):
 
 @admin.register(MyNews)
 class MyNewsAdmin(admin.ModelAdmin):
-    list_display = ("title", "display_name", "published", "created_at")
+    list_display = (
+        "title",
+        "display_name",
+        "published",
+        "created_at",
+    )
     list_filter = ("published",)
     search_fields = ("title",)
     ordering = ("-created_at",)
--- app/home/models.py
+++ app/home/models.py
@@ -5,6 +5,47 @@ from django.db import models
 from .managers import MyNewsManager
 
 
+class TikTokUser(models.Model):
+    id = models.AutoField(primary_key=True)
+    open_id = models.CharField(blank=True, max_length=255)
+    access_token = models.CharField(blank=True, max_length=255)
+    refresh_token = models.CharField(blank=True, max_length=255)
+    expires_in = models.DateTimeField(null=True)
+
+    display_name = models.CharField(blank=True, max_length=255)
+    avatar_url = models.URLField(blank=True, max_length=510)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.display_name or self.open_id
+
+    def __repr__(self):
+        return f"<id={self.id} name={self.display_name} open_id={self.open_id}"
+
+    class Meta:
+        verbose_name = "TikTok User"
+        verbose_name_plural = "TikTok Users"
+        # ordering = ["-display_name"]
+
+
+class BetaUser(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(verbose_name="E-Mail", unique=True)
+    name = models.CharField(max_length=255, verbose_name="Name")
+    details = models.TextField(verbose_name="Details", blank=True)
+    added = models.BooleanField(verbose_name="Added", default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} - {}".format(self.name[:16], self.email[:16])
+
+    class Meta:
+        verbose_name = "Beta User"
+        verbose_name_plural = "Beta Users"
+
+
 class Message(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=32, verbose_name="Name")
